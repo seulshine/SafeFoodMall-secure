5ecca9178708c3db17beb86fe4bca6288c47a68f
student <student@M801>
2019-11-04 13:53:40 +0900

    branch

---
+++ src/main/java/com/ssafy/controller/FoodRestController.java
@@ -0,0 +1,40 @@
+package com.ssafy.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.edu.vue.dto.EmployeeDto;
+import com.ssafy.service.FoodService;
+import com.ssafy.vo.Food;
+import com.ssafy.vo.FoodPageBean;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/food")
+@Slf4j
+public class FoodRestController {
+
+	@Autowired
+	FoodService fservice;
+	
+	@GetMapping("/getAllFoods")
+	public Object getAllFoods() {
+		log.trace("getAllFoods is called");
+		try {
+			FoodPageBean fpb = new FoodPageBean("all", "", "", 0);
+			List<Food> foods = fservice.searchAll(bean);
+			return new ResponseEntity<List<EmployeeDto>>(emps, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error("findAllEmployees", e);
+			throw e;		// 얘를 호출한놈은 spring. 이걸 톰캣이 클라이언트한테 에러로 보낸다.
+		}
+	}
+	
+}
